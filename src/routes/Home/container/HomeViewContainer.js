3eb661f46973035c9a75f6fbb1d569988f66a7c1
MityaDSCH <mlsgearon@gmail.com>
2016-07-28 16:13:32 -0500

    Oops, duplicated folder during rename

--- src/routes/Home/container/HomeViewContainer.js
+++
@@ -1,6 +0,0 @@
-import { connect } from 'react-redux'
-import { actions } from '../../../components/Menu/modules/menu'
-
-import HomeView from '../components/HomeView'
-
-export default connect(state => state, {animateInMenu: actions.animateInMenu})(HomeView)
