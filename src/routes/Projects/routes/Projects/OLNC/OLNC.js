24863b50769664d727c802dbf5785e89b0a7adf4
MityaDSCH <mlsgearon@gmail.com>
2016-08-06 16:11:56 -0500

    Add OLNC text

--- src/routes/Projects/routes/Projects/OLNC/OLNC.js
+++ src/routes/Projects/routes/Projects/OLNC/OLNC.js
@@ -71,14 +71,14 @@ export default class ScoreFluent extends React.Component {
           <div className={classes.linksContainer}>
             <a
               className={classes.siteLink}
-              href='http://fcc-market.herokuapp.com'
+              href='http://oudinglegalnurse.com'
               target='_blank'>
-              fcc-market.herokuapp.com
+              oudinglegalnurse.com
             </a>
             |
             <a
               className={classes.githubLogoContainer}
-              href='http://github.com/mityadsch/fcc-market'
+              href='http://github.com/mityadsch/OLNC3'
               target='_blank'>
               <Isvg
                 src={GithubLogo}
@@ -88,28 +88,10 @@ export default class ScoreFluent extends React.Component {
             </a>
           </div>
           <p className={classes.description}>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Iure minima
-            ullam officia ut cum iusto veritatis in officiis, facilis ratione
-            excepturi unde delectus itaque eaque vitae voluptatem, sit nobis nostrum
-            consequuntur maxime pariatur, eligendi cupiditate. Quidem laborum, ut,
-            autem iusto corporis illum, nesciunt eos eligendi ipsum tempore expedita,
-            harum maxime.
-          </p>
-          <p className={classes.description}>
-            Quisquam eveniet adipisci cupiditate, in eum deleniti ex suscipit error
-            doloribus odio. Numquam voluptatibus fuga similique animi minima
-            consectetur omnis, tempore, itaque culpa. Facilis ipsa facere voluptatibus
-            enim voluptates nemo iste totam in iure sint inventore, esse voluptate
-            laboriosam velit, fugit, maxime possimus itaque quidem quibusdam odit
-            voluptas modi id!
-          </p>
-          <p className={classes.description}>
-            Esse quos optio voluptatibus voluptate nostrum reprehenderit earum
-            inventore. Ducimus voluptas maxime ipsum libero. Maiores aliquam, hic
-            ducimus libero non distinctio vero ex provident, alias omnis! Soluta
-            corporis, architecto voluptates quo beatae, facere, quasi eos laudantium
-            voluptatibus quibusdam veniam consequuntur tempora nostrum porro nulla
-            repudiandae quidem dolorem. Voluptates, molestiae, reprehenderit.
+            This is the very first website I ever made, and I've got to say, I
+            think it holds up decently in that context! I was asked to make a
+            neutral design and logo as well as a fully responsive site. And
+            simple as it may be, it seems to do that well.
           </p>
           <hr className={classes.stackRule} />
           <h2>Tools</h2>
