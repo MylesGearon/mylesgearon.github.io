a110ea2da373e0b24ba40a4292d74018fc9b8bc5
MityaDSCH <mlsgearon@gmail.com>
2016-08-06 17:13:20 -0500

    Reorder projects

--- src/routes/Projects/routes/Projects/index.js
+++ src/routes/Projects/routes/Projects/index.js
@@ -12,6 +12,12 @@ export default [
     description: 'A Musical Sight Reading App'
   },
   {
+    dirname: 'OLNC',
+    path: 'olnc',
+    title: 'Ouding Consulting',
+    description: 'A Static Site for Ouding Legal Nurse Consulting'
+  },
+  {
     dirname: 'TheLearningCenter',
     path: 'the-learning-center',
     title: 'The Learning Center',
@@ -23,10 +29,4 @@ export default [
     title: 'FCC Stock Tracker',
     description: 'A Live Updating Stock Comparison Project'
   },
-  {
-    dirname: 'OLNC',
-    path: 'olnc',
-    title: 'Ouding Consulting',
-    description: 'A Static Site for Ouding Legal Nurse Consulting'
-  }
 ]
