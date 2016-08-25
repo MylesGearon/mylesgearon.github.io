60c3894a8f32fd5774807f4456b9e6a8817bbf54
MityaDSCH <mlsgearon@gmail.com>
2016-08-25 14:30:42 -0500

    Add Codepen example to Meta page

--- src/routes/Projects/routes/Projects/Meta/component.js
+++ src/routes/Projects/routes/Projects/Meta/component.js
@@ -30,22 +30,29 @@ export default {
 
     `I began learning web dev using FreeCodeCamp's curriculum, and one of the
     very fist things they have you do is create a portfolio site (<a
-    href="https://codepen.io/FreeCodeCamp/pen/YqLyXB">example</a>).  I passed up
-    that assignment and finished nearly the entire curriculum before putting
-    much thought into making my own because I wanted to know everything possible
-    before I started it, I needed to have the heavens open up in praise when I
-    pushed it to production. And that's how I ended up not having a portfolio
-    for a year. Oops. One of the most valuable things I've learned recently is
-    the idea of making a Minimum Viable Product instead of trying to perfect
-    everything about a project on the first pass. So here we are, warts and all
-    (looking at you (with great resentment) IE), but I'm constantly polishing.`,
+    href="https://codepen.io/FreeCodeCamp/pen/YqLyXB"
+    target="_blank">example</a>).  I passed up that assignment and finished
+    nearly the entire curriculum before putting much thought into making my own
+    because I wanted to know everything possible before I started it, I needed
+    to have the heavens open up in praise when I pushed it to production. And
+    that's how I ended up not having a portfolio for a year. Oops. One of the
+    most valuable things I've learned recently is the idea of making a Minimum
+    Viable Product instead of trying to perfect everything about a project on
+    the first pass, so I'm constantly polishing.`,
 
     `At the end of this project I wanted to feel like I'd really wrapped my head
     around all of the most common tools in the React workflow, so I learned some
     React Router and Redux then used the wonderful boilerplate <a
-    href="https://github.com/davezuko/react-redux-starter-kit#features"> React
-    Redux Starter Kit</a> which had me learn some Koa, testing tools, and CSS
-    Modules besides.`
+    href="https://github.com/davezuko/react-redux-starter-kit#features"
+    target="_blank"> React Redux Starter Kit</a> which had me learn some Koa,
+    testing tools, and CSS Modules besides.`,
+
+    `If you like the crazy flying text that I use for the menu and titles on
+    this site, you can play with the code on CodePen <a
+    href="http://codepen.io/MityaDSCH/pen/pbapwN" target="_blank">here</a>.
+    Maybe I went a bit overboard with it on this site (when you have a
+    hammer...), but I think it's really fun and it was even featured on
+    CodePen's front page!`
 
   ],
 
