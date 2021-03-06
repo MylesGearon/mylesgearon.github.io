228f1bf5a7ebd5a9f19d2b94749b6b68f41f65a3
Myles <myles@mylesgearon.com>
2021-03-06 17:49:48 -0500

    Remove old training project listings

--- src/routes/Projects/routes/Projects/index.js
+++ src/routes/Projects/routes/Projects/index.js
@@ -23,28 +23,28 @@ export default [
     title: 'mylesgearon.com',
     description: 'An Overwrought SPA Portfolio'
   },
-  {
-    dirname: 'ScoreFluent',
-    path: 'score-fluent',
-    title: 'Score Fluent',
-    description: 'A Musical Sight Reading App'
-  },
-  {
-    dirname: 'OLNC',
-    path: 'olnc',
-    title: 'Ouding Consulting',
-    description: 'A Static Site for Ouding Legal Nurse Consulting'
-  },
-  {
-    dirname: 'TheLearningCenter',
-    path: 'the-learning-center',
-    title: 'The Learning Center',
-    description: 'A Static Site for an Adult Eduction Nonprofit'
-  },
-  {
-    dirname: 'FCCMarket',
-    path: 'fcc-market',
-    title: 'FCC Stock Tracker',
-    description: 'A Live Updating Stock Comparison Project'
-  }
+  // {
+  //   dirname: 'ScoreFluent',
+  //   path: 'score-fluent',
+  //   title: 'Score Fluent',
+  //   description: 'A Musical Sight Reading App'
+  // },
+  // {
+  //   dirname: 'OLNC',
+  //   path: 'olnc',
+  //   title: 'Ouding Consulting',
+  //   description: 'A Static Site for Ouding Legal Nurse Consulting'
+  // },
+  // {
+  //   dirname: 'TheLearningCenter',
+  //   path: 'the-learning-center',
+  //   title: 'The Learning Center',
+  //   description: 'A Static Site for an Adult Eduction Nonprofit'
+  // },
+  // {
+  //   dirname: 'FCCMarket',
+  //   path: 'fcc-market',
+  //   title: 'FCC Stock Tracker',
+  //   description: 'A Live Updating Stock Comparison Project'
+  // }
 ]
